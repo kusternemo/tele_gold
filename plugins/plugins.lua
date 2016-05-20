4c1153697984bc0550a21333c0c02c1f52a6ef3e
tele gold <mohmadrsol81@gmail.com>
2016-05-20 14:06:09 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -211,8 +211,6 @@ return {
     "^!plugins$",
     "^!plugins? (enable) ([%w_%.%-]+)$",
     "^!plugins? (disable) ([%w_%.%-]+)$",
-    "^!plugins? (enable) ([%w_%.%-]+) (chat)",
-    "^!plugins? (disable) ([%w_%.%-]+) (chat)",
     "^!plugins? (reload)$" },
   run = run,
   moderated = true, -- set to moderator mode
