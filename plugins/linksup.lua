f6a77854331d4faa9eff76570dd4732fcd31dc97
tele gold <mohmadrsol81@gmail.com>
2016-05-21 13:49:56 +0430

    Update linksup.lua

--- plugins/linksup.lua
+++ plugins/linksup.lua
@@ -2,11 +2,11 @@ do
 
 function run(msg, matches)
     local data = load_data(_config.moderation.data)
-      local group_link = data[tostring(123229873)]['settings']['set_link']
+      local group_link = data[tostring(1053132857)]['settings']['set_link']
        if not group_link then
       return ''
        end
-         local text = "لینک ساپورت:\n" ..group_link
+         local text = "لینک ساپورت:\n" . https://telegram.me/joinchat/Bg2_-T7FiDnyI9sv-9e5Rg
  return "👈"..text
 end
 
